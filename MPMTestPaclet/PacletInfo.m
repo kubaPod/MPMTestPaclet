4f64535988c7d15c7b7e6a2bfd096b1082360579
Kuba Podkalicki <kuba.pod@gmail.com>
2017-09-29 21:45:19 +0200

    regular release

--- MPMTestPaclet/PacletInfo.m
+++ MPMTestPaclet/PacletInfo.m
@@ -1,4 +1,4 @@
 (* Created with the Wolfram Language : www.wolfram.com *)
-Paclet[Name -> "MPMTestPaclet", Version -> "0.0.3", 
+Paclet[Name -> "MPMTestPaclet", Version -> "0.0.4", 
  MathematicaVersion -> "11+", Description -> "", 
  Creator -> "Jakub (Kuba) Podkalicki", Extensions -> {}]
